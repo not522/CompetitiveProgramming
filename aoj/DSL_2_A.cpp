ea313b2274798b93cf08064ebc8b6eac45ca179f
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-06-19 22:46:44 +0900

    [AOJ] solve DSL_2_A

---
+++ aoj/DSL_2_A.cpp
@@ -0,0 +1,14 @@
+#include "template.hpp"
+#include "rmq.hpp"
+
+int main() {
+  int n, q;
+  cin >> n >> q;
+  RMQ<int> rmq(n);
+  for (int i = 0; i < q; ++i) {
+    int com, x, y;
+    cin >> com >> x >> y;
+    if (com == 0) rmq.update(x, y);
+    else cout << rmq.query(x, y + 1) << endl;
+  }
+}
