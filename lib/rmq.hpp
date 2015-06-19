792b5820e9d6661add1eebf377ab0aa0ea2de883
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-06-19 22:44:32 +0900

    [library] add RMQ

---
+++ lib/rmq.hpp
@@ -0,0 +1,11 @@
+#include "segment_tree.hpp"
+
+template<typename T> class RMQ : public SegmentTree<T> {
+protected:
+  T function(T l, T r) {
+    return min(l, r);
+  }
+  
+public:
+  RMQ(int n) : SegmentTree<T>(n) {}
+};
