d5662855c5c4d6341c6cc5c863b7c248f6ff8f47
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-06-19 20:03:59 +0900

    [AOJ] solve DSL_1

---
+++ aoj/DSL_1.cpp
@@ -0,0 +1,17 @@
+#include "template.hpp"
+#include "union_find.hpp"
+
+int main() {
+  int n, q;
+  cin >> n >> q;
+  UnionFind uf(n);
+  for (int i = 0; i < q; ++i) {
+    int com, x, y;
+    cin >> com >> x >> y;
+    if (com == 0) {
+      uf.unite(x, y);
+    } else {
+      cout << (uf.equal(x, y) ? 1 : 0) << endl;
+    }
+  }
+}
