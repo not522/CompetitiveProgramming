7e03dd13437aacc64c6aea5b4dcc20ec11b37510
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-06-20 08:34:31 +0900

    [library] remove prev (shortest path)

--- lib/shortest_path.hpp
+++ lib/shortest_path.hpp
@@ -5,7 +5,7 @@ private:
   typedef __typeof(Edge::cost) Cost;
 
   struct State {
-    int pos, prv;
+    int pos;
     Cost cost;
     bool operator<(const State& state) const {return cost > state.cost;}
   };
@@ -13,27 +13,23 @@ private:
 public:
   const Cost INF;
 
-  vector<vector<int>> prev;
   vector<Cost> dist;
 
   ShortestPath(const Graph<Edge>& graph) : Graph<Edge>(graph), INF(numeric_limits<Cost>::max()) {}
   
   void run (int from) {
     priority_queue<State> que;
-    que.push((State){from, -1, 0});
-    prev.resize(this->edges.size());
+    que.push((State){from, 0});
     dist.resize(this->edges.size(), INF);
     
     while (!que.empty()) {
       State now = que.top();
       que.pop();
-      if (dist[now.pos] < now.cost) continue;
-      prev[now.pos].emplace_back(now.prv);
-      if (dist[now.pos] == now.cost) continue;
+      if (dist[now.pos] <= now.cost) continue;
       dist[now.pos] = now.cost;
       for (const Edge& edge : this->edges[now.pos]) {
         if (dist[edge.to] < now.cost + edge.cost) continue;
-        que.push((State){edge.to, now.pos, now.cost + edge.cost});
+        que.push((State){edge.to, now.cost + edge.cost});
       }
     }
   }
