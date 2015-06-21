d37873055d1f41159a3712837a3c72b0585dadcf
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-06-22 03:54:08 +0900

    [library] add combination

--- AtCoder/ABC003/D.cpp
+++ AtCoder/ABC003/D.cpp
@@ -1,17 +1,10 @@
 #include "mint.hpp"
-
-Mint nCk(int n, int k) {
-  if (n < k) return 0;
-  Mint res = 1;
-  for (int i = 1; i <= n; ++i) res *= i;
-  for (int i = 1; i <= k; ++i) res /= i;
-  for (int i = 1; i <= n - k; ++i) res /= i;
-  return res;
-}
+#include "combination.hpp"
 
 int main() {
   int r, c, x, y, d, l;
   cin >> r >> c >> x >> y >> d >> l;
+  Combination<Mint> combination;
   Mint k = 0;
   for (int i1 = 0; i1 <= 1; ++i1) {
     for (int i2 = 0; i2 <= 1; ++i2) {
@@ -20,7 +13,7 @@ int main() {
         for (int i4 = 0; i4 <= 1; ++i4) {
           if (y <= i3 + i4) continue;
           int p = (i1 + i2 + i3 + i4) % 2 ? -1 : 1;
-          k += p * nCk((x - i1 - i2) * (y - i3 - i4), d) * nCk((x - i1 - i2) * (y - i3 - i4) - d, l);
+          k += p * combination.combination((x - i1 - i2) * (y - i3 - i4), d) * combination.combination((x - i1 - i2) * (y - i3 - i4) - d, l);
         }
       }
     }
---
+++ lib/combination.hpp
@@ -0,0 +1,30 @@
+#include "template.hpp"
+
+template<typename T> class Combination {
+private:
+  vector<vector<T>> comb;
+  
+public:
+  Combination(int n = 0) : comb(n, vector<T>(n, 0)) {
+    for (int i = 0; i < n; ++i) comb[i][0] = 1;
+    for (int i = 1; i < n; ++i) {
+      for (int j = 1; j < n; ++j) {
+        comb[i][j] = comb[i - 1][j] + comb[i - 1][j - 1];
+      }
+    }
+  }
+
+  T combination(int n, int m) {
+    if (n < m) return 0;
+    if (n < (int)comb.size()) return comb[n][m];
+    T res = 1;
+    for (int i = 1; i <= n; ++i) res *= i;
+    for (int i = 1; i <= m; ++i) res /= i;
+    for (int i = 1; i <= n - m; ++i) res /= i;
+    return res;
+  }
+
+  T repetition(int n, int r) {
+    return combination(n + r - 1, r);
+  }
+};
