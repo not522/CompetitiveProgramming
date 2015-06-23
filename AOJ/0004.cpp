0827eb5ae959f1f64024b4e3a05302d2863e7d75
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-06-23 16:43:58 +0900

    [AOJ] solve 0004

---
+++ AOJ/0004.cpp
@@ -0,0 +1,10 @@
+#include "math/square_matrix.hpp"
+
+int main() {
+  SquareMatrix<double> m(2);
+  Vector<double> v(2);
+  while (cin >> m[0][0] >> m[0][1] >> v[0] >> m[1][0] >> m[1][1] >> v[1]) {
+    auto res = m.inverse() * v;
+    cout << fixed << setprecision(3) << res[0] << " " << fixed << setprecision(3) << res[1] << endl;
+  }
+}
