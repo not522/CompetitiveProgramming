f47877df58338c9fd7bc479416e63fd04251eb42
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-05 11:05:05 +0900

    fix compile error

--- AtCoder/ABC004/D.cpp
+++ AtCoder/ABC004/D.cpp
@@ -11,13 +11,13 @@ int main() {
   for (int& i : box) i = id++;
   ResidualGraph<WeightedResidualEdge<int, int>> graph(id);
   for (int i = 0; i < 3; ++i) {
-    graph.add_edge(WeightedResidualEdge<int, int>(source, v[i], n[i], 0));
+    graph.addEdge(WeightedResidualEdge<int, int>(source, v[i], n[i], 0));
     for (int j = 0; j < 2000; ++j) {
-      graph.add_edge(WeightedResidualEdge<int, int>(v[i], box[j], 1, abs((i * 100 - 100) - (j - 1000))));
+      graph.addEdge(WeightedResidualEdge<int, int>(v[i], box[j], 1, abs((i * 100 - 100) - (j - 1000))));
     }
   }
   for (int i = 0; i < 2000; ++i) {
-    graph.add_edge(WeightedResidualEdge<int, int>(box[i], sink, 1, 0));
+    graph.addEdge(WeightedResidualEdge<int, int>(box[i], sink, 1, 0));
   }
   MinCostFlow<int, int> mcf(graph);
   cout << mcf.solve(source, sink, accumulate(n.begin(), n.end(), 0)) << endl;
