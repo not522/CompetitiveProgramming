c85246321522eef1e4ebd960550c94c2128576f7
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-12 01:11:11 +0900

    fix compile error

--- AtCoder/ABC026/B.cpp
+++ AtCoder/ABC026/B.cpp
@@ -1,12 +1,12 @@
-#include "geometry/const.hpp"
+#include "template.hpp"
 
 int main() {
   int n;
   cin >> n;
-  vector<Real> r(n);
+  vector<double> r(n);
   for (auto& i : r) cin >> i;
   sort(r.rbegin(), r.rend());
-  Real res = 0;
-  for (int i = 0; i < n; ++i) res += (i % 2 ? -1 : 1) * r[i] * r[i] * PI;
+  double res = 0;
+  for (int i = 0; i < n; ++i) res += (i % 2 ? -1 : 1) * r[i] * r[i] * M_PI;
   cout << fixed << setprecision(15) << res << endl;
 }
