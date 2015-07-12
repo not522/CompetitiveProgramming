260ae3a686a2c958cbb7033334068c33271c137b
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-13 08:50:45 +0900

    refactor

--- AOJ/Volume0/0007.cpp
+++ AOJ/Volume0/0007.cpp
@@ -5,7 +5,7 @@ int main() {
   cin >> n;
   int k = 100000;
   for (int i = 0; i < n; ++i) {
-    k = ceil(k * 105 / 100, 1000);
+    k = ceil(k * 105 / 100, 1000) * 1000;
   }
   cout << k << endl;
 }
