8fda089410873722f833b811c50851a34ae29452
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-13 21:50:24 +0900

    [AtCoder] Dwango 2015 Prelims B

---
+++ AtCoder/dwango2015-prelims/B.cpp
@@ -0,0 +1,14 @@
+#include "string/replace_all.hpp"
+
+int main() {
+  string s;
+  cin >> s;
+  replaceAll(s, "25", ".");
+  for (char& c : s) {
+    if (isdigit(c)) c = ' ';
+  }
+  stringstream ss(s);
+  long long res = 0;
+  while (ss >> s) res += s.size() * (s.size() + 1) / 2;
+  cout << res << endl;
+}
