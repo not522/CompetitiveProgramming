d59c3fc58775da5e2964d74b4a750a7a6ddb32c7
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-15 00:19:07 +0900

    [yukicoder] solve 146

---
+++ yukicoder/146.cpp
@@ -0,0 +1,13 @@
+#include "math/mint.hpp"
+
+int main() {
+  int n;
+  cin >> n;
+  Mint res = 0;
+  for (int i = 0; i < n; ++i) {
+    long long c, d;
+    cin >> c >> d;
+    res += Mint((c + 1) / 2) * d;
+  }
+  cout << res << endl;
+}
