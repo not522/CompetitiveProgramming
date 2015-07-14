7dab0e4b63a189c1aa83523149072529e048b605
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-15 02:41:14 +0900

    [yukicoder] solve 227

---
+++ yukicoder/227.cpp
@@ -0,0 +1,15 @@
+#include "template.hpp"
+
+int main() {
+  array<int, 13> c{0};
+  for (int i = 0; i < 5; ++i) {
+    int a;
+    cin >> a;
+    ++c[a - 1];
+  }
+  if (count(c.begin(), c.end(), 3) && count(c.begin(), c.end(), 2)) cout << "FULL HOUSE" << endl;
+  else if (count(c.begin(), c.end(), 3)) cout << "THREE CARD" << endl;
+  else if (count(c.begin(), c.end(), 2) == 2) cout << "TWO PAIR" << endl;
+  else if (count(c.begin(), c.end(), 2)) cout << "ONE PAIR" << endl;
+  else cout << "NO HAND" << endl;
+}
