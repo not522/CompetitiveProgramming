7287777cc2458f8de6cda87232cc06ed17c15a7b
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-14 18:51:44 +0900

    [yukicoder] solve 18

---
+++ yukicoder/18.cpp
@@ -0,0 +1,9 @@
+#include "math/mint.hpp"
+
+int main() {
+  Mint::setMod(26);
+  string s;
+  cin >> s;
+  for (int i = 0; i < (int)s.size(); ++i) cout << (char)('A' + Mint(s[i] - 'A' - i - 1));
+  cout << endl;
+}
