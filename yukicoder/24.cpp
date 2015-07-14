632bb0bba415e14adc699357955570e45bec6841
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-14 19:20:40 +0900

    [yukicoder] solve 24

---
+++ yukicoder/24.cpp
@@ -0,0 +1,24 @@
+#include "template.hpp"
+
+struct Question {
+  array<int, 4> v;
+  bool res;
+};
+
+int main() {
+  int n;
+  cin >> n;
+  vector<Question> q(n);
+  for (int i = 0; i < n; ++i) {
+    for (int& j : q[i].v) cin >> j;
+    string s;
+    cin >> s;
+    q[i].res = (s == "YES");
+  }
+  for (int i = 0; i < 10; ++i) {
+    auto f = [&](Question q){return (find(q.v.begin(), q.v.end(), i) != q.v.end()) == q.res;};
+    if (all_of(q.begin(), q.end(), f)) {
+      cout << i << endl;
+    }
+  }
+}
