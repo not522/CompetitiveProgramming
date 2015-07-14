516d6d0afa2f3015ba644ce84dc09197ddb290d9
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-14 13:55:59 +0900

    fix compile error

--- AtCoder/abc026/C.cpp
+++ AtCoder/abc026/C.cpp
@@ -4,7 +4,7 @@
 
 class Salary : public TreeDP<Tree<Edge>, int> {
 private:
-  int visit(int v, vector<int> children) {
+  int visit(int v, const vector<int>& children) {
     if (children.empty()) return 1;
     auto m = minmax_element(children.begin(), children.end());
     return *m.first + *m.second + 1;
