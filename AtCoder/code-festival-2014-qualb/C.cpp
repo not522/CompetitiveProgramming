825c2a1e6acbb27e2ba4fcda5b6efb7b3a04e846
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-14 12:28:04 +0900

    [AtCoder] solve CODE FESTIVAL 2014 QualB C

---
+++ AtCoder/code-festival-2014-qualb/C.cpp
@@ -0,0 +1,22 @@
+#include "template.hpp"
+
+int main() {
+  string s[3];
+  for (auto& i : s) cin >> i;
+  map<char, int> m[3];
+  for (int i = 0; i < 3; ++i) {
+    for (char c : s[i]) ++m[i][c];
+  }
+  int a = 0, b = 0;
+  for (const auto& i : m[2]) {
+    char c = i.first;
+    int v = i.second;
+    if (m[0][c] + m[1][c] < v) {
+      cout << "NO" << endl;
+      return 0;
+    }
+    a += max(0, v - m[1][c]);
+    b += max(0, v - m[0][c]);
+  }
+  cout << (a <= (int)s[0].size() / 2 && b <= (int)s[0].size() / 2 ? "YES" : "NO") << endl;
+}
