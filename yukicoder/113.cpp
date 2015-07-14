c3bb8e5331abe659247e4a5fb00b83585f76c8db
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-14 21:25:51 +0900

    [yukicoder] solve 113

---
+++ yukicoder/113.cpp
@@ -0,0 +1,16 @@
+#include "template.hpp"
+
+int main() {
+  string s;
+  cin >> s;
+  int x = 0, y = 0;
+  for (char c : s) {
+    switch(c) {
+    case 'N': ++y; break;
+    case 'E': ++x; break;
+    case 'W': --x; break;
+    case 'S': --y; break;
+    }
+  }
+  cout << fixed << setprecision(15) << sqrt(y * y + x * x) << endl;
+}
