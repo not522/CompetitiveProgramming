896510bd40f9698312aafc38c40bef13a573fc9b
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-15 01:25:27 +0900

    [yukicoder] solve 185

---
+++ yukicoder/185.cpp
@@ -0,0 +1,13 @@
+#include "template.hpp"
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; ++i) {
+    int x, y;
+    cin >> x >> y;
+    v[i] = y - x;
+  }
+  cout << (count(v.begin(), v.end(), v[0]) == (int)v.size() && v[0] > 0 ? v[0] : -1) << endl;
+}
