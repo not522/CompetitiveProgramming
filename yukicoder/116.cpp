6b86e0ccfbb79c9f61ff085ebf83a601af880378
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-14 23:34:06 +0900

    [yukicoder] solve 116

---
+++ yukicoder/116.cpp
@@ -0,0 +1,16 @@
+#include "template.hpp"
+
+int main() {
+  int n, res = 0;
+  cin >> n;
+  vector<int> a(n);
+  for (int& i : a) cin >> i;
+  for (int i = 0; i < n - 2; ++i) {
+    auto itr = a.begin() + i;
+    if (set<int>(itr, itr + 3).size() != 3u) continue;
+    auto m = minmax_element(itr, itr + 3);
+    if (m.first != itr && m.second != itr) ++res;
+    else if (m.first != itr + 2 && m.second != itr + 2) ++res;
+  }
+  cout << res << endl;
+}
