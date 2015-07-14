5d9e13d804354d9d667efafd0bd5c99c872eb8a4
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-14 20:38:04 +0900

    [yukicoder] solve 82

---
+++ yukicoder/82.cpp
@@ -0,0 +1,14 @@
+#include "template.hpp"
+
+int main() {
+  int w, h;
+  char c;
+  cin >> w >> h >> c;
+  for (int i = 0; i < h; ++i) {
+    for (int j = 0; j < w; ++j) {
+      if ((i + j) % 2) cout << (char)(c ^ 'B' ^ 'W');
+      else cout << c;
+    }
+    cout << endl;
+  }
+}
