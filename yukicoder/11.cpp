7571848b7bb4a38a76da584c1a648536523491cf
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-16 19:28:14 +0900

    [yukicoder] solve 11

---
+++ yukicoder/11.cpp
@@ -0,0 +1,14 @@
+#include "template.hpp"
+
+int main() {
+  long long w, h, n;
+  cin >> w >> h >> n;
+  set<int> a, b;
+  for (int i = 0; i < n; ++i) {
+    int s, k;
+    cin >> s >> k;
+    a.insert(s);
+    b.insert(k);
+  }
+  cout << a.size() * h + b.size() * w - a.size() * b.size() - n << endl;
+}
