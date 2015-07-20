01156edfc7bd55c0d5e4c7d0e59a7e620adc0852
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-20 10:53:53 +0900

    refactor

--- lib/dp/knapsack.hpp
+++ lib/dp/knapsack.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "template.hpp"
 
-template<typename Weight, typename Value, typename Function> Value knapsack(Weight maxWeight, const vector<Weight>& weight, const vector<Value>& value, Value init = 0, Function func = plus<Weight>()) {
+template<typename Weight, typename Value> Value knapsack(Weight maxWeight, const vector<Weight>& weight, const vector<Value>& value, Value init = 0, function<Weight(Weight, Weight)> func = plus<Weight>()) {
   vector<Value> dp(maxWeight + 1, 0);
   dp[0] = init;
   for (size_t i = 0; i < weight.size(); ++i) {
