396a7679c6c4890a0b109c57a4533f0163712809
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-23 02:21:48 +0900

    [yukicoder] solve 106

---
+++ yukicoder/106.cpp
@@ -0,0 +1,11 @@
+#include "math/prime.hpp"
+
+int main() {
+  int n, k, res = 0;
+  cin >> n >> k;
+  Prime prime(n + 1);
+  for (int i = 2; i <= n; ++i) {
+    if ((int)prime.primeFactor(i).size() >= k) ++res;
+  }
+  cout << res << endl;
+}
