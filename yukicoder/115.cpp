c457fbc4cb62b31793cf98f58a8c4e142932fe79
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-23 03:42:12 +0900

    [yukicoder] solve 115

---
+++ yukicoder/115.cpp
@@ -0,0 +1,17 @@
+#include "template.hpp"
+
+int main() {
+  int n, d, k;
+  cin >> n >> d >> k;
+  if (d < (k + 1) * k / 2 || (2 * n - k + 1) * k / 2 < d) {
+    cout << -1 << endl;
+    return 0;
+  }
+  for (int i = 1; k; ++i) {
+    if (d <= i + (2 * n - k + 2) * (k - 1) / 2) {
+      d -= i;
+      --k;
+      cout << i << (k ? " " : "\n");
+    }
+  }
+}
