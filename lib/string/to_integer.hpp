a4805e3b466a617c9d8a1ee4f33916c4e6669884
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-07-28 10:19:48 +0900

    [yukicoder] solve 164

--- lib/string/to_integer.hpp
+++ lib/string/to_integer.hpp
@@ -14,3 +14,12 @@ template<> inline long toInteger<long>(const string& s) {
 template<> inline long long toInteger<long long>(const string& s) {
   return stoll(s);
 }
+
+template<typename T = long long> inline T toInteger(const string& s, int n) {
+  T res = 0;
+  for (char c : s) {
+    if (isdigit(c)) res = res * n + c - '0';
+    else if (isalpha(c)) res = res * n + tolower(c) - 'a' + 10;
+  }
+  return s[0] == '-' ? -res : res;
+}
---
+++ yukicoder/164.cpp
@@ -0,0 +1,18 @@
+#include "string/to_integer.hpp"
+
+int main() {
+  int n;
+  cin >> n;
+  long long res = numeric_limits<long long>::max();
+  for (int i = 0; i < n; ++i) {
+    string v;
+    cin >> v;
+    int mx = 0;
+    for (char c : v) {
+      if (isdigit(c)) mx = max(mx, c - '0' + 1);
+      else if (isalpha(c)) mx = max(mx, c - 'A' + 11);
+    }
+    res = min(res, toInteger(v, mx));
+  }
+  cout << res << endl;
+}
