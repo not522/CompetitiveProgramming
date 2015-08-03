ec4dcfaf14d7e24c9f20c28d1e0c43c06d3e4558
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-04 07:49:35 +0900

    fix compile error

--- yukicoder/49.cpp
+++ yukicoder/49.cpp
@@ -4,7 +4,7 @@ int main() {
   string s;
   cin >> s;
   Parser<> parser;
-  parser.addOperator(plus<int>(), '*', 0);
-  parser.addOperator(multiplies<int>(), '+', 0);
+  parser.addBinaryOperator(plus<int>(), '*', 0);
+  parser.addBinaryOperator(multiplies<int>(), '+', 0);
   cout << parser.parse(s) << endl;
 }
