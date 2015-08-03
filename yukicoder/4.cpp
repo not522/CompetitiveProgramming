bafcbf8be9596862961a1738263f5c1941f0a6b7
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-03 23:39:44 +0900

    fix compile error

--- yukicoder/4.cpp
+++ yukicoder/4.cpp
@@ -6,5 +6,5 @@ int main() {
   vector<int> w(n);
   for (int& i : w) cin >> i;
   static const int sum = accumulate(w.begin(), w.end(), 0);
-  cout << (sum % 2 == 0 && knapsack(sum / 2, w, vector<int>(n, 1), 1, multiplies<int>()) ? "possible" : "impossible") << endl;
+  cout << (sum % 2 == 0 && knapsack_fill(sum / 2, w).back() ? "possible" : "impossible") << endl;
 }
