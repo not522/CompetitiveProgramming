08539ea208836f07141818fbb826834f6ae75b9c
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-04 00:19:25 +0900

    [yukicoder] solve 204

---
+++ yukicoder/204.cpp
@@ -0,0 +1,20 @@
+#include "string/split.hpp"
+
+int main() {
+  int d;
+  cin >> d;
+  string s1, s2;
+  cin >> s1 >> s2;
+  string s = string(14, 'x') + s1 + s2 + string(14, 'x');
+  auto v = split(s, 'x');
+  auto res = max_element(v.begin(), v.end())->size();
+  for (size_t i = 0; i < s.size(); ++i) {
+    string ss = s;
+    for (int j = 0; j < d && i + j < s.size() && s[i + j] == 'x'; ++j) {
+      ss[i + j] = 'o';
+      auto v = split(ss, 'x');
+      res = max(res, max_element(v.begin(), v.end())->size());
+    }
+  }
+  cout << res << endl;
+}
