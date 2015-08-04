19866b16266f3f89f9262e12fa594faa1e13c59a
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-04 17:43:12 +0900

    [yukicoder] solve 9

---
+++ yukicoder/9.cpp
@@ -0,0 +1,27 @@
+#include "template.hpp"
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n), b(n);
+  for (int& i : a) cin >> i;
+  for (int& i : b) cin >> i;
+  int res = numeric_limits<int>::max();
+  for (int i = 0; i < n; ++i) {
+    priority_queue<tuple<int, int>, vector<tuple<int, int>>, greater<tuple<int, int>>> que;
+    for (int i = 0; i < n; ++i) que.push(make_tuple(a[i], 0));
+    for (int i = 0; i < n; ++i) {
+      auto p = que.top();
+      que.pop();
+      que.push(make_tuple(get<0>(p) + b[i] / 2, get<1>(p) + 1));
+    }
+    int mx = 0;
+    while (!que.empty()) {
+      mx = max(mx, get<1>(que.top()));
+      que.pop();
+    }
+    res = min(res, mx);
+    rotate(b.begin(), b.begin() + 1, b.end());
+  }
+  cout << res << endl;
+}
