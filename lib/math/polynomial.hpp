973a45d65fa3a2eb191c1776d637bbb10cbe3e6f
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-04 17:52:13 +0900

    [Library] add Karatsuba

--- lib/math/polynomial.hpp
+++ lib/math/polynomial.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "arithmetic.hpp"
+#include "math/fft.hpp"
 
 template<typename T> class Polynomial : public Arithmetic<Polynomial<T>>, public arithmetic::Modulus<Polynomial<T>> {
 private:
@@ -18,6 +19,8 @@ public:
 	Polynomial(const vector<T>& val) : val(val) {
     normalize();
   }
+
+  Polynomial(const int n) : val(vector<T>(n)) {}
 
   Polynomial(const int n, const T val) : val(vector<T>(n, val)) {}
 
@@ -39,18 +42,31 @@ public:
 		return *this;
 	}
 
-  // TODO FFT
 	Polynomial operator*=(const Polynomial& p) {
-		Polynomial res;
-    for (int i = 0; i < size(); ++i) {
-      for (int j = 0; j < p.size(); ++j) {
-        res[i + j] += val[i] * p[j];
+    int a = size(), b = p.size();
+    if (min(a, b) <= 256) {
+      Polynomial res;
+      for (int i = 0; i < a; ++i) {
+        for (int j = 0; j < b; ++j) res[i + j] += val[i] * p[j];
       }
+      *this = res;
+      normalize();
+      return *this;
     }
-		*this = res;
+    int m = (max(a, b) + 1) / 2;
+    Polynomial a0(m), a1(m), b0(m), b1(m);
+    for (int i = 0; i < min(m, a); ++i) a0[i] = (*this)[i];
+    for (int i = m; i < a; ++i) a1[i - m] = (*this)[i];
+    for (int i = 0; i < min(m, b); ++i) b0[i] = p[i];
+    for (int i = m; i < b; ++i) b1[i - m] = p[i];
+    auto z0 = a0 * b0, z2 = a1 * b1, z1 = z0 + z2 - (a1 - a0) * (b1 - b0);
+    val = vector<T>(a + b);
+    for (int i = 0; i < z0.size(); ++i) (*this)[i] += z0[i]; 
+    for (int i = 0; i < z1.size(); ++i) (*this)[i + m] += z1[i]; 
+    for (int i = 0; i < z2.size(); ++i) (*this)[i + 2 * m] += z2[i]; 
     normalize();
     return *this;
-	}
+  }
 
   Polynomial operator/=(const Polynomial& p) {
     Polynomial res;
