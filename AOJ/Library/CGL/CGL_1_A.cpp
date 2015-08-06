ec784c762e31501d8a2854201c78b923c470c1c2
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-06 10:46:36 +0900

    fix compile error

--- AOJ/Library/CGL/CGL_1_A.cpp
+++ AOJ/Library/CGL/CGL_1_A.cpp
@@ -1,14 +1,14 @@
 #include "geometry/projection.hpp"
 
 int main() {
-  Point p1, p2;
-  cin >> p1 >> p2;
+  Line line;
+  cin >> line;
   int q;
   cin >> q;
   for (int i = 0; i < q; ++i) {
     Point p, r;
     cin >> p;
-    r = perpendicular(Line(p1, p2), p);
+    r = projection(line, p);
     cout << r << endl;
   }
 }
