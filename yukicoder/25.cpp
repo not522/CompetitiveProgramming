c4491e857a314cf3595603065bb8571a7727b416
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-06 10:36:57 +0900

    [yukicoder] solve 25

---
+++ yukicoder/25.cpp
@@ -0,0 +1,23 @@
+#include "math/basic.hpp"
+
+int main() {
+  long long n, m;
+  cin >> n >> m;
+  auto g = gcd(n, m);
+  n /= g;
+  m /= g;
+  while (n % 10 == 0) n /= 10;
+  n %= 10;
+  while (m != 1) {
+    n *= 10;
+    g = gcd(n, m);
+    if (g == 1) {
+      cout << -1 << endl;
+      return 0;
+    }
+    n /= g;
+    m /= g;
+    n %= 10;
+  }
+  cout << n << endl;
+}
