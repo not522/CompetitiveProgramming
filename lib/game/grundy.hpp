3c5cc419bfed6e2491d5cb1b711af564d413d4f0
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-16 09:43:59 +0900

    [yukicoder] solve 153

---
+++ lib/game/grundy.hpp
@@ -0,0 +1,22 @@
+#include "template.hpp"
+
+template<typename T> class Grundy {
+private:
+  map<T, int> grundy;
+
+  template<typename S> int solve(S s) {
+    int g = 0;
+    for (auto t : s) g ^= solve(t);
+    return g;
+  }
+
+public:
+  int solve(T t) {
+    if (grundy.count(t)) return grundy[t];
+    set<int> s;
+    for (auto g : t.next()) s.insert(solve(g));
+    for (int i = 0; ; ++i) {
+      if (s.count(i) == 0) return grundy[t] = i;
+    }
+  }
+};
