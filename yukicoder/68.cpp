a491b048e43fd2eeb883790c415fb796b83dd49d
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-17 20:32:06 +0900

    [yukicoder] solve 68

---
+++ yukicoder/68.cpp
@@ -0,0 +1,30 @@
+#include "bisection_method.hpp"
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> l(n);
+  for (int& i : l) cin >> i;
+  auto f = [&](double x){
+    long long sum = 0;
+    for (int& i : l) sum += floor(i / x);
+    return sum - 500000 + 0.5;
+  };
+  auto mx =  bisectionMethod(f, numeric_limits<int>::max(), 0);
+  vector<long double> res;
+  for (const int& i : l) {
+    for (int j = 1; ; ++j) {
+      long double d = (long double)i / j;
+      if (d < mx - 1e-9) break;
+      res.emplace_back(d);
+    }
+  }
+  sort(res.rbegin(), res.rend());
+  int q;
+  cin >> q;
+  for (int i = 0; i < q; ++i) {
+    int k;
+    cin >> k;
+    cout << fixed << setprecision(15) << res[k - 1] << endl;
+  }
+}
