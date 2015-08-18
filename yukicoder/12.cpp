58af2b72339820e3780d0f9fbaa73fe7581b35b7
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-19 00:46:00 +0900

    [yukicoder] solve 12

---
+++ yukicoder/12.cpp
@@ -0,0 +1,27 @@
+#include "math/prime.hpp"
+
+int main() {
+  Prime prime(5000000);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int& i : a) cin >> i;
+  set<int> b(a.begin(), a.end()), c;
+  int res = -1, p = 1;
+  for (int i = 1; i <= 5000000; ++i) {
+    if (!prime.isPrime(i)) continue;
+    bool ok = true;
+    for (int ii = i; ii; ii /= 10) {
+      if (b.count(ii % 10) == 0) ok = false;
+    }
+    if (ok) {
+      for (int ii = i; ii; ii /= 10) c.insert(ii % 10);
+    } else {
+      if (b == c) res = max(res, i - 1 - p);
+      p = i + 1;
+      c = set<int>();
+    }
+  }
+  if (b == c) res = max(res, 5000000 - p);
+  cout << res << endl;
+}
