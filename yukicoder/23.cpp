6b84f25df5fa5d4704664c8c514fdab69affe139
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-19 02:45:45 +0900

    [yukicoder] solve 23

---
+++ yukicoder/23.cpp
@@ -0,0 +1,9 @@
+#include "template.hpp"
+
+int main() {
+  int h, a, d;
+  cin >> h >> a >> d;
+  map<int, double> dp;
+  for (int i = 1; i <= h; ++i) dp[i] = min(dp[i - a] + 1, dp[i - d] + 1.5);
+  cout << fixed << setprecision(15) << dp[h] << endl;
+}
