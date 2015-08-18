dae7e13d93c1c397465d6dd87e44c3f3b2c4a5fe
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-18 22:09:40 +0900

    [yukicoder] solve 258

---
+++ yukicoder/258.cpp
@@ -0,0 +1,30 @@
+// TODO refactoring
+
+#include "string/to_string.hpp"
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for (int& i : v) cin >> i;
+  vector<int> dp(n + 1), prv(n + 1, -1);
+  dp[1] = v[0];
+  for (int i = 1; i < n; ++i) {
+    if (dp[i] > dp[i - 1] + v[i]) {
+      dp[i + 1] = dp[i];
+      prv[i + 1] = i;
+    } else {
+      dp[i + 1] = dp[i - 1] + v[i];
+      prv[i + 1] = i - 1;
+    }
+  }
+  int t = n;
+  vector<int> res;
+  while (t > 0) {
+    if (prv[t] != t - 1) res.emplace_back(t);
+    t = prv[t];
+  }
+  reverse(res.begin(), res.end());
+  cout << dp.back() << endl;
+  cout << to_string(res) << endl;
+}
