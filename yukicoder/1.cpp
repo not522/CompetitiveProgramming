3470fce06df30396158dd83eb8eccf848c3f332b
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-18 21:02:00 +0900

    [yukicoder] solve 1

---
+++ yukicoder/1.cpp
@@ -0,0 +1,34 @@
+// TODO refactoring
+
+#include "template.hpp"
+
+struct Edge {
+  int to, cost, dist;
+
+  Edge() {}
+
+  Edge(int to, int cost, int dist) : to(to), cost(cost), dist(dist) {}
+};
+
+int main() {
+  int n, c, v;
+  cin >> n >> c >> v;
+  vector<int> s(v), t(v), y(v), m(v);
+  vector<vector<Edge>> edge(n);
+  for (int& i : s) cin >> i;
+  for (int& i : t) cin >> i;
+  for (int& i : y) cin >> i;
+  for (int& i : m) cin >> i;
+  for (int i = 0; i < v; ++i) edge[s[i] - 1].emplace_back(Edge(t[i] - 1, y[i], m[i]));
+  const int INF = numeric_limits<int>::max() / 2;
+  vector<vector<int>> dp(n, vector<int>(c + 1, INF));
+  fill(dp[0].begin(), dp[0].end(), 0);
+  for (int i = 0; i < n; ++i) {
+    for (const auto& e : edge[i]) {
+      for (int j = 0; j <= c - e.cost; ++j) {
+        dp[e.to][j + e.cost] = min(dp[e.to][j + e.cost], dp[i][j] + e.dist);
+      }
+    }
+  }
+  cout << (dp[n - 1][c] != INF ? dp[n - 1][c] : -1) << endl;
+}
