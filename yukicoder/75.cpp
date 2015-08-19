389cb0f0549ecf10644fe78e01e92e762f9d5c47
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-08-19 21:17:35 +0900

    [yukicoder] solve 75

---
+++ yukicoder/75.cpp
@@ -0,0 +1,20 @@
+#include "template.hpp"
+
+int main() {
+  int k;
+  cin >> k;
+  vector<double> dp1(k + 6), dp2(k + 6);
+  dp1[0] = 1;
+  for (int i = 1; i < k + 6; ++i) {
+    for (int j = 1; j <= 6; ++j) {
+      if (i - j < 0 || k <= i - j) continue;
+      dp1[i] += dp1[i - j] / 6;
+      dp2[i] += dp1[i - j] / 6 * (dp2[i - j] + 1);
+    }
+    dp2[i] /= dp1[i];
+  }
+  double t = 0;
+  for (int i = k + 1; i < k + 6; ++i) t += dp1[i] * dp2[i];
+  t /= accumulate(dp1.begin() + k + 1, dp1.end(), 0.);
+  cout << fixed << setprecision(15) << dp2[k] + t * (1 - dp1[k]) / dp1[k] << endl;
+}
