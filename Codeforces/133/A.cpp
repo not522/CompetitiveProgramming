165a7eaca0ed802af25467a7b41ef49cfe3a4200
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-10-07 18:21:23 +0900

    [Codeforces] solve 133 A

---
+++ Codeforces/133/A.cpp
@@ -0,0 +1,8 @@
+#include "template.hpp"
+
+int main() {
+  string s;
+  cin >> s;
+  if (s.find('H') != string::npos || s.find('Q') != string::npos || s.find('9') != string::npos) cout << "YES" << endl;
+  else cout << "NO" << endl;
+}
