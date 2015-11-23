abf3631df436b526c585a89c081ad2004bbe286c
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-11-23 16:07:15 +0900

    [AOJ] solve 0029

---
+++ AOJ/Volume0/0029.cpp
@@ -0,0 +1,16 @@
+#include "template.hpp"
+
+int main() {
+  string s;
+  int mx1 = 0, mx2 = 0;
+  map<string, int> m;
+  while (cin >> s) ++m[s];
+  for (auto i : m) mx1 = max(mx1, i.second);
+  for (auto i : m) mx2 = max(mx2, (int)i.first.size());
+  for (auto i : m) {
+    if (i.second == mx1) cout << i.first << " ";
+  }
+  for (auto i : m) {
+    if ((int)i.first.size() == mx2) cout << i.first << endl;
+  }
+}
