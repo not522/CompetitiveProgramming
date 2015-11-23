5d05549b46f1d84f65145615cbc98b0ac403c8c8
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-11-23 12:07:30 +0900

    [Codeforces] solve 469 A

---
+++ Codeforces/469/A.cpp
@@ -0,0 +1,14 @@
+#include "template.hpp"
+
+int main() {
+  int n, p, q;
+  cin >> n >> p;
+  vector<int> a(p);
+  for (int& i : a) cin >> i;
+  cin >> q;
+  vector<int> b(q);
+  for (int& i : b) cin >> i;
+  set<int> s(a.begin(), a.end());
+  s.insert(b.begin(), b.end());
+  cout << (n == (int)s.size() ? "I become the guy." : "Oh, my keyboard!") << endl;
+}
