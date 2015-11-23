234c40c545b98d054c4828b638e06d7fb7e4b0f1
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-11-23 19:42:09 +0900

    [AtCoder] solve ARC009 A

---
+++ AtCoder/arc009/A.cpp
@@ -0,0 +1,13 @@
+#include "template.hpp"
+
+int main() {
+  int n;
+  cin >> n;
+  int sum = 0;
+  for (int i = 0; i < n; ++i) {
+    int a, b;
+    cin >> a >> b;
+    sum += a * b;
+  }
+  cout << (int)(sum * 1.05) << endl;
+}
