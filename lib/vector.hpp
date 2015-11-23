80d214776c85419e426776e0af2ad73ac899272c
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-11-23 22:09:24 +0900

    [Library] add vector sort and reverse

--- lib/vector.hpp
+++ lib/vector.hpp
@@ -10,3 +10,15 @@ template <typename T> inline ostream& operator<<(ostream &s, const vector<T> &v)
   for (T &t : v) s << t << endl;
   return s;
 }
+
+template<typename T> void sort(vector<T>& v) {
+  sort(v.begin(), v.end());
+}
+
+template<typename T> void rsort(vector<T>& v) {
+  sort(v.rbegin(), v.rend());
+}
+
+template<typename T> void reverse(vector<T>& v) {
+  reverse(v.begin(), v.end());
+}
