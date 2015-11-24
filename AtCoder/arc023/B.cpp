d368bae4f8e76b9236bf30db74f4965e019800dc
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-11-24 18:48:11 +0900

    [AtCoder] solve ARC023 B

---
+++ AtCoder/arc023/B.cpp
@@ -0,0 +1,15 @@
+#include "minmax.hpp"
+#include "vector.hpp"
+
+int main() {
+  int r, c, d, res = 0;
+  cin >> r >> c >> d;
+  vector<vector<int>> a(r, vector<int>(c));
+  cin >> a;
+  for (int i = 0; i < r; ++i) {
+    for (int j = 0; j < c; ++j) {
+      if (i + j <= d && (i + j) % 2 == d % 2) chmax(res, a[i][j]);
+    }
+  }
+  cout << res << endl;
+}
