c813cd25f423a5aab29b427c20b0b67ca551f063
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2015-11-26 03:10:57 +0900

    [AtCoder] solve ARC022 B

---
+++ AtCoder/arc022/B.cpp
@@ -0,0 +1,16 @@
+#include "minmax.hpp"
+#include "vector.hpp"
+
+int main() {
+  int n, res = 0;
+  cin >> n;
+  vector<int> a(n);
+  cin >> a;
+  set<int> s;
+  for (int i = 0, j = 0; i < n; ++i) {
+    while (s.count(a[i])) s.erase(a[j++]);
+    s.emplace(a[i]);
+    chmax(res, i - j + 1);
+  }
+  cout << res << endl;
+}
