c28a2f05867654327bbbc3ea06e2dbcbb655284b
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2016-02-14 22:03:16 +0900

    refactor

--- AtCoder/dwango2016-prelims/D.cpp
+++ AtCoder/dwango2016-prelims/D.cpp
@@ -1,4 +1,5 @@
 #include "cumulative_sum_2D.hpp"
+#include "minmax.hpp"
 #include "vector.hpp"
 
 long long solve(const vector<vector<long long>>& b) {
@@ -8,16 +9,10 @@ long long solve(const vector<vector<long long>>& b) {
   vector<long long> dp1(w + 1, -1e18), dp2(w + 1, -1e18);
   for (int i = 0; i < h; ++i) for (int j = i + 1; j <= h; ++j) {
     vector<long long> d1(w + 1), d2(w + 1);
-    for (int k = 0; k < w; ++k) {
-      long long a = sum.sum(i, k, j, k + 1);
-      dp1[k] = max(dp1[k], d1[k + 1] = max(d1[k] + a, a));
-    }
-    for (int k = w - 1; k >= 0; --k) {
-      long long a = sum.sum(i, k, j, k + 1);
-      dp2[k] = max(dp2[k], d2[k] = max(d2[k + 1] + a, a));
-    }
+    for (int k = 0; k < w; ++k) chmax(dp1[k], d1[k + 1] = max(d1[k], 0ll) + sum.sum(i, k, j, k + 1));
+    for (int k = w - 1; k >= 0; --k) chmax(dp2[k], d2[k] = max(d2[k + 1], 0ll) + sum.sum(i, k, j, k + 1));
   }
-  for (int i = 0; i < w; ++i) for (int j = 0; j < i; ++j) for (int k = i; k < w; ++k) res = max(res, dp1[j] + dp2[k]);
+  for (int i = 0; i < w; ++i) for (int j = 0; j < i; ++j) for (int k = i; k < w; ++k) chmax(res, dp1[j] + dp2[k]);
   return res;
 }
 
