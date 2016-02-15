92fd30c4a48d8a31aad1dbaff8885510f1234cf5
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2016-02-15 18:46:10 +0900

    [AtCoder] solve ARC024 B

---
+++ AtCoder/arc024/B.cpp
@@ -0,0 +1,21 @@
+#include "minmax.hpp"
+#include "vector.hpp"
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> c(n);
+  cin >> c;
+  if (!in(c, 0) || !in(c, 1)) {
+    cout << -1 << endl;
+    return 0;
+  }
+  c.insert(c.end(), c.begin(), c.end());
+  int cnt = 0, res = 0;
+  for (int i = 1; i < 2 * n; ++i) {
+    if (c[i] != c[i - 1]) cnt = 0;
+    else ++cnt;
+    chmax(res, cnt / 2 + 1);
+  }
+  cout << res << endl;
+}
