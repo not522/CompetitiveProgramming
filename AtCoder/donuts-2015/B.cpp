899fc8bccef4353307be3ee094279de8cb22e282
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2016-03-12 22:23:22 +0900

    [AtCoder] solve Donuts 2015 B

---
+++ AtCoder/donuts-2015/B.cpp
@@ -0,0 +1,28 @@
+#include "minmax.hpp"
+#include "vector.hpp"
+#include "container/fix_size_set.hpp"
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> a(n), b(m), c(m);
+  vector<vector<int>> v(m);
+  cin >> a;
+  for (int i = 0; i < m; ++i) {
+    cin >> b[i] >> c[i];
+    v[i].resize(c[i]);
+    cin >> v[i];
+  }
+  int res = 0;
+  for (auto i : FixSizeSet(n, 9)) {
+    int r = 0;
+    for (int j : i) r += a[j];
+    for (int j = 0; j < m; ++j) {
+      int cnt = 0;
+      for (int k : v[j]) if (i.in(k - 1)) ++cnt;
+      if (cnt >= 3) r += b[j];
+    }
+    chmax(res, r);
+  }
+  cout << res << endl;
+}
