25697bf77a74a1971cba038f0a445ae1bd46b396
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2016-03-26 22:34:12 +0900

    [AtCoder] solve CODE FESTIVAL 2014 morning easy A

---
+++ AtCoder/code-festival-2014-morning-easy/A.cpp
@@ -0,0 +1,10 @@
+#include "vector.hpp"
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  cin >> a;
+  adjacent_difference(a, a);
+  cout << (double)(accumulate(a) - a[0]) / (n - 1) << endl;
+}
