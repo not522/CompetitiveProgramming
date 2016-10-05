c0f9b1c1d23fb551bf61b3a888b3c83c268b87ee
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2016-10-06 00:02:31 +0900

    [AtCoder] solve ABC045 B

---
+++ AtCoder/abc045/B.cpp
@@ -0,0 +1,19 @@
+#include "template.hpp"
+
+int main() {
+  string s[3];
+  int t = 0;
+  for (auto& i : s) {
+    cin >> i;
+    reverse(i.begin(), i.end());
+  }
+  while (true) {
+    if (s[t].empty()) {
+      cout << (char)(t + 'A') << endl;
+      return 0;
+    }
+    int p = s[t].back() - 'a';
+    s[t].pop_back();
+    t = p;
+  }
+}
