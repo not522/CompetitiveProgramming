d663edb6f01a8507491eb0ed13e82014fea09c01
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2016-10-21 22:05:59 +0900

    fix LIBRARY_PATH

--- import.py
+++ import.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 
-library_path = os.getenv('LIBRARY_PATH')
+library_path = os.getenv('LIBRARY_PATH').split(':')[-1]
 
 def getHeader(file, headers):
     for raw_line in open(file):
