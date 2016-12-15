4cd4a82a84cf4fea8ccc3bfad2238e250eeb611f
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2016-12-16 01:40:11 +0900

    [yukicoder] 375

---
+++ yukicoder/375.cpp
@@ -0,0 +1,14 @@
+#include "minmax.hpp"
+#include "math/prime.hpp"
+
+int main() {
+  int64_t n, mn = numeric_limits<int64_t>::max();
+  cin >> n;
+  Prime prime;
+  for (auto i : prime.divisor(n)) {
+    for (auto j : prime.divisor(i)) {
+      chmin(mn, n / i + i / j + j - 3);
+    }
+  }
+  cout << mn << " " << n - 1 << endl;
+}
