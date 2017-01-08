898b55ab2bc3cab68d036dc5a088877e10151181
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2017-01-09 02:54:47 +0900

    variable template

--- lib/adjacent_loop.hpp
+++ lib/adjacent_loop.hpp
@@ -3,8 +3,7 @@
 
 template<int N> class AdjacentLoop {
 private:
-  static int dy(int i);
-  static int dx(int i);
+  const static array<int, N> dy, dx;
 
   struct Iterator {
     const int y, x, h, w;
@@ -12,20 +11,20 @@ private:
 
     Iterator(int y, int x, int h, int w, int val) : y(y), x(x), h(h), w(w), val(val) {
       for (; this->val < N; ++this->val) {
-        int yy = y + dy(this->val);
-        int xx = x + dx(this->val);
+        int yy = y + dy[this->val];
+        int xx = x + dx[this->val];
         if (0 <= yy && yy < h && 0 <= xx && xx < w) break;
       }
     }
 
-    pair<int, int> operator*() {return make_pair(y + dy(val), x + dx(val));}
+    pair<int, int> operator*() {return make_pair(y + dy[val], x + dx[val]);}
 
     bool operator!=(Iterator& itr) {return val < itr.val;}
 
     void operator++() {
       while (++val < N) {
-        int yy = y + dy(val);
-        int xx = x + dx(val);
+        int yy = y + dy[val];
+        int xx = x + dx[val];
         if (0 <= yy && yy < h && 0 <= xx && xx < w) break;
       }
     }
@@ -41,52 +40,17 @@ public:
   Iterator& end() {return n;}
 };
 
-template<> int AdjacentLoop<2>::dy(int i) {
-  constexpr int dy[]{0, 1};
-  return dy[i];
-}
+template<> const array<int, 2> AdjacentLoop<2>::dy{0, 1};
+template<> const array<int, 2> AdjacentLoop<2>::dx{1, 0};
 
-template<> int AdjacentLoop<2>::dx(int i) {
-  constexpr int dx[]{1, 0};
-  return dx[i];
-}
+template<> const array<int, 4> AdjacentLoop<4>::dy{0, -1, 0, 1};
+template<> const array<int, 4> AdjacentLoop<4>::dx{1, 0, -1, 0};
 
-template<> int AdjacentLoop<4>::dy(int i) {
-  constexpr int dy[]{0, -1, 0, 1};
-  return dy[i];
-}
+template<> const array<int, 5> AdjacentLoop<5>::dy{0, -1, 0, 1, 0};
+template<> const array<int, 5> AdjacentLoop<5>::dx{1, 0, -1, 0, 0};
 
-template<> int AdjacentLoop<4>::dx(int i) {
-  constexpr int dx[]{1, 0, -1, 0};
-  return dx[i];
-}
+template<> const array<int, 8> AdjacentLoop<8>::dy{-1, -1, -1, 0, 0, 1, 1, 1};
+template<> const array<int, 8> AdjacentLoop<8>::dx{-1, 0, 1, -1, 1, -1, 0, 1};
 
-template<> int AdjacentLoop<5>::dy(int i) {
-  constexpr int dy[]{0, -1, 0, 1, 0};
-  return dy[i];
-}
-
-template<> int AdjacentLoop<5>::dx(int i) {
-  constexpr int dx[]{1, 0, -1, 0, 0};
-  return dx[i];
-}
-
-template<> int AdjacentLoop<8>::dy(int i) {
-  constexpr int dy[]{-1, -1, -1, 0, 0, 1, 1, 1};
-  return dy[i];
-}
-
-template<> int AdjacentLoop<8>::dx(int i) {
-  constexpr int dx[]{-1, 0, 1, -1, 1, -1, 0, 1};
-  return dx[i];
-}
-
-template<> int AdjacentLoop<9>::dy(int i) {
-  constexpr int dy[]{-1, -1, -1, 0, 0, 0, 1, 1, 1};
-  return dy[i];
-}
-
-template<> int AdjacentLoop<9>::dx(int i) {
-  constexpr int dx[]{-1, 0, 1, -1, 0, 1, -1, 0, 1};
-  return dx[i];
-}
+template<> const array<int, 9> AdjacentLoop<9>::dy{-1, -1, -1, 0, 0, 0, 1, 1, 1};
+template<> const array<int, 9> AdjacentLoop<9>::dx{-1, 0, 1, -1, 0, 1, -1, 0, 1};
