2b7eecfd0cdc15337ea0a50a9ff8153a5fa1c3ea
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2017-01-31 04:58:27 +0900

    test string/split

--- lib/string/split.hpp
+++ lib/string/split.hpp
@@ -4,6 +4,8 @@
 vector<string> split(const string &str, char delim = ' ') {
   stringstream ss(str);
   vector<string> res;
-  for (string tmp; getline(ss, tmp, delim);) res.push_back(tmp);
+  for (string tmp; getline(ss, tmp, delim);) {
+    if (tmp != "") res.push_back(tmp);
+  }
   return res;
 }
