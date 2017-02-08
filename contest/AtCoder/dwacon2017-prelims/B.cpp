ffac00239cc4188e242e4d5d439e05fd0a0372e9
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2017-02-09 01:37:12 +0900

    [AtCoder] dwacon2017 prelims B

---
+++ contest/AtCoder/dwacon2017-prelims/B.cpp
@@ -0,0 +1,26 @@
+#include "minmax.hpp"
+
+int main() {
+  string t;
+  cin >> t;
+  int res = 0, two = 0, five = 0;
+  for (char c : t) {
+    if (c == '2') {
+      two = five + 1;
+      five = 0;
+    } else if (c == '5') {
+      five = two ? two + 1 : 0;
+      chmax(res, five);
+      two = 0;
+    } else if (c == '?') {
+      int t = two;
+      two = five + 1;
+      five = t ? t + 1 : 0;
+      chmax(res, five);
+    } else {
+      two = 0;
+      five = 0;
+    }
+  }
+  cout << res << endl;
+}
