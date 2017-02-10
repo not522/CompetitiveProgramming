1f393488f01f2d8147440cc07f4eebf965cd24b7
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2017-02-11 03:09:18 +0900

    template

--- include/container/bit.hpp
+++ include/container/bit.hpp
@@ -1,20 +1,20 @@
 #pragma once
 #include "template.hpp"
 
-class BIT {
+template<typename T> class BIT {
 private:
-  vector<int> v;
+  vector<T> v;
 
 public:
   BIT() {}
 
-  BIT(int n) : v(vector<int>(n + 1, 0)) {}
+  BIT(int n) : v(vector<T>(n + 1, 0)) {}
 
   // [0, i)
-  int sum(int i) {return i ? v[i] + sum(i & (i - 1)) : 0;}
+  T sum(int i) {return i ? v[i] + sum(i & (i - 1)) : 0;}
 
   // [a, b)
-  int sum(int a, int b) {return sum(b) - sum(a);}
+  T sum(int a, int b) {return sum(b) - sum(a);}
 
-  void add(int i, int x) {for (++i; i < int(v.size()); i += i & -i) v[i] += x;}
+  void add(int i, T x) {for (++i; i < int(v.size()); i += i & -i) v[i] += x;}
 };
