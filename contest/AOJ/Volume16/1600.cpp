a6210ad9ca5b221300db7d5b9cb8a9b205a040f3
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2017-10-05 22:57:39 +0900

    [AOJ] 1600

---
+++ contest/AOJ/Volume16/1600.cpp
@@ -0,0 +1,16 @@
+#include "minmax.hpp"
+#include "vector.hpp"
+
+int main() {
+  while (true) {
+    int m, nmin, nmax, r = 0, res = -1;
+    cin >> m >> nmin >> nmax;
+    if (m == 0 && nmin == 0 && nmax == 0) break;
+    vector<int> p(m);
+    cin >> p;
+    for (int i = nmax; i >= nmin; --i) {
+      if (chmax(r, p[i - 1] - p[i])) res = i;
+    }
+    cout << res << endl;
+  }
+}
