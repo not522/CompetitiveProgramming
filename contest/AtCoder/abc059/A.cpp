f705718839cc8b213f30698e4e8d3a730aeb91f2
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2018-01-27 21:38:11 +0900

    fix old-style cast

--- contest/AtCoder/abc059/A.cpp
+++ contest/AtCoder/abc059/A.cpp
@@ -3,5 +3,5 @@
 int main() {
   string a, b, c;
   cin >> a >> b >> c;
-  cout << (char)toupper(a[0]) << (char)toupper(b[0]) << (char)toupper(c[0]) << endl;
+  cout << char(toupper(a[0])) << char(toupper(b[0])) << char(toupper(c[0])) << endl;
 }
