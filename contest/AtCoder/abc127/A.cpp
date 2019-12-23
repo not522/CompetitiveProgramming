7bc87782a6fff862e089298952c491e9c51d8021
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2019-12-23 18:47:14 +0900

    [AtCoder] ABC127 A

---
+++ contest/AtCoder/abc127/A.cpp
@@ -0,0 +1,12 @@
+#include "template.hpp"
+
+int main() {
+  int a(in), b(in);
+  if (a >= 13) {
+    cout << b << endl;
+  } else if (a >= 6) {
+    cout << b / 2 << endl;
+  } else {
+    cout << 0 << endl;
+  }
+}
