31366a585b2c4acf6523429388dc54aa4d26eaee
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2020-02-15 22:52:17 +0900

    [AtCoder] ABC154 A

---
+++ contest/AtCoder/abc154/A.cpp
@@ -0,0 +1,12 @@
+#include "string.hpp"
+
+int main() {
+  String s(in), t(in);
+  int a(in), b(in);
+  String u(in);
+  if (s == u) {
+    cout << a - 1 << " " << b << endl;
+  } else {
+    cout << a << " " << b - 1 << endl;
+  }
+}
