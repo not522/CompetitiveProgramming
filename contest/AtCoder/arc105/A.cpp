0da9be57d86800a49416b4e93ac98d41c0b7b71c
Naoto Mizuno <naotomizuno@preferred.jp>
2020-10-12 09:35:59 +0900

    format

--- contest/AtCoder/arc105/A.cpp
+++ contest/AtCoder/arc105/A.cpp
@@ -3,5 +3,7 @@
 int main() {
   Vector<int> v(4, in);
   v.sort();
-  cout << (2 * (v.min() + v.max()) == v.accumulate() || 2 * v.max() == v.accumulate()) << endl;
+  cout << (2 * (v.min() + v.max()) == v.accumulate() ||
+           2 * v.max() == v.accumulate())
+       << endl;
 }
