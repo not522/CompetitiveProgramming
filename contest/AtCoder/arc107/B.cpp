104938382c2322178012d8dc9e78594d5cc1e2fb
Naoto Mizuno <gobou522@gmail.com>
2020-11-05 09:56:41 +0900

    format

--- contest/AtCoder/arc107/B.cpp
+++ contest/AtCoder/arc107/B.cpp
@@ -3,7 +3,7 @@
 int main() {
   int64_t n(in), k(in), res = 0;
   for (int64_t i = -n + 1; i <= n - 1; ++i) {
-    int64_t j  = k - i;
+    int64_t j = k - i;
     if (j < -n + 1 || n - 1 < j) {
       continue;
     }
